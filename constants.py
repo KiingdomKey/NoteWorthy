e5955f1b214ca102a7d024bcb012fc6fc3ef3a24
KiingdomKey <allthegoodemailshavebeentaken1@gmail.com>
2026-05-31 12:00:50 -0500

    Initial commit

---
+++ constants.py
@@ -0,0 +1,7 @@
+NOTE_ONSET_SYMBOL = 1
+NOTE_CONTINUED_SYMBOL = 2
+NOTE_END_SYMBOL = 3
+
+LOWEST_NOTE_OFFSET = 20 + 1 # 20 is A0 in pretty_midi, and the extra 1 is for the indexing offset
+CQT_NUM_BUCKETS = 88
+MIN_NOTE_INTERVAL = 0.1
